5d3841b1e9cad6b54cd3954c958ccbf51b4dafbb
cherubina-gabriak <a.naumovich2017@yandex.ru>
2024-04-20 14:04:12 +0300

    add LeafNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -16,6 +16,25 @@ class TestHTMLNode(unittest.TestCase):
     def test_raises(self):
         node = HTMLNode("h1", "This is a title")
         self.assertRaises(NotImplementedError, node.to_html)
+
+class TestleafNode(unittest.TestCase):
+    def test_to_html(self):
+        node = LeafNode("a", "This is a link", {"target": "_blank", "href": "#", "id": "link" })
+        html = node.to_html()
+
+        expected = '<a target="_blank" href="#" id="link">This is a link</a>'
+
+        self.assertEqual(html, expected)
+
+    def test_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError, node.to_html)
+
+    def test_no_tag(self):
+        node = LeafNode(None, "This is raw text")
+        html = node.to_html()
+        self.assertEqual(html, "This is raw text")
+
 
 if __name__ == "__main__":
     unittest.main()
