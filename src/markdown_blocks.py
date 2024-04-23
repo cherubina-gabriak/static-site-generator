d824d7679005916721a86264646b68ffa7f43d39
cherubina-gabriak <a.naumovich2017@yandex.ru>
2024-04-23 22:54:18 +0300

    add block to block type fn

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,12 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
+
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")    
     filtered_blocks = []
@@ -9,4 +18,41 @@ def markdown_to_blocks(markdown):
         filtered_blocks.append(block)
 
     return filtered_blocks
+
+def block_to_block_type(block):
+    lines = block.split("\n")
+
+    if not re.match(r"^#{1,6}", block) is None:
+        return block_type_heading
+    
+    if len(lines) > 1 and lines[0] == "```" and lines[len(lines) - 1] == "```":
+        return block_type_code
+
+    if block.startswith(">"):
+        for line in lines:
+            if not line.startswith(">"):
+                return block_type_paragraph
+        return block_type_quote
+
+    if block.startswith("* "):
+        for line in lines:
+            if not line.startswith("* "):
+                return block_type_paragraph
+        return block_type_unordered_list
+
+    if block.startswith("- "):
+        for line in lines:
+            if not line.startswith("- "):
+                return block_type_paragraph
+        return block_type_unordered_list
+
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return block_type_paragraph
+            i += 1
+        return block_type_ordered_list
+
+    return block_type_paragraph
 
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -1,6 +1,13 @@
 import unittest
 
-from markdown_blocks import markdown_to_blocks
+from markdown_blocks import (
+    block_to_block_type, 
+    block_type_heading, 
+    markdown_to_blocks, 
+    block_type_paragraph, 
+    block_type_ordered_list,
+    block_type_quote
+)
 
 class TestBlockMarkdown(unittest.TestCase):
     def test_markdown_to_blocks(self):
@@ -12,3 +19,13 @@ class TestBlockMarkdown(unittest.TestCase):
             "* This is a list\n* with items"
         ]
         self.assertEqual(blocks, expected)
+
+    def test_block_to_block_type(self):
+        heading = "## This is heading 2"
+        self.assertEqual(block_to_block_type(heading), block_type_heading)
+        ol = "1. First item\n2. Second item"
+        self.assertEqual(block_to_block_type(ol), block_type_ordered_list)
+        para = "1. First item\n2. Second item\n4. This is not third item"
+        self.assertEqual(block_to_block_type(para), block_type_paragraph)
+        quote = ">this is quote"
+        self.assertEqual(block_to_block_type(quote), block_type_quote)
