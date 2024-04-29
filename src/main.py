d86b89aee52722bd1382e43e349ab6d76b0fd69d
cherubina-gabriak <a.naumovich2017@yandex.ru>
2024-04-29 21:09:58 +0300

    add html generation

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 import shutil
 import os
 from copystatic import copy_file_tree
-from generate_page import generate_page
+from generate_page import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
 
 def main():
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
     copy_file_tree(dir_path_static, dir_path_public)
-    generate_page("./content/index.md", "./template.html", f"{dir_path_public}/index.html")
+    generate_pages_recursive(dir_path_content, "./template.html", dir_path_public)
 
 main()
